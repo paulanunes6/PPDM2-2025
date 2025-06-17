c1e4da73224fbae6c210f1e71e47c83eafe500b7
Paula Rangel <138053971+paulanunes6@users.noreply.github.com>
2025-06-17 14:50:10 -0300

    ex FORMULADEGET

---
+++ a formula do get/app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+const porta = 3000
+
+app.set("view engine","ejs")
+
+app.get("/", (req,res)=>{
+    res.render("conta facil")
+})
+
+app.get("/resultado", (req,res)=>{
+    let numero = Number(req.query.numero)
+    let total = numero**2
+    res.render('resultado', {total})
+})
+
+app.listen(porta, ()=>{
+    console.log('servidor aberto na página https://localhost:3000:${porta}')
+})
